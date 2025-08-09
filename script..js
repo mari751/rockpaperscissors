6aad447979e04fac6b619f4ab299ead7376913b9
mari751 <mariamdumbadze40@gmail.com>
2025-08-09 23:48:41 +0400

    Add files via upload

---
+++ script..js
@@ -0,0 +1,57 @@
+
+       //lesson 1
+
+// alert("good morning!");
+// alert("mari");
+
+// console.log(20 - 5);
+// console.log(2 + 2 - 5);
+
+// document.body.innerHTML = "good morning!";
+// document.body.innerHTML += "<h1>marrriii</h1>";
+
+// let shirt = 10;
+// let socks = 8;
+// let plate = 20;
+
+// console.log(shirt + socks + plate);
+
+// let start = 100;
+// let lunch = 20;
+// let dinner = 50;
+// let earning = 200;
+
+// let spending = (lunch + dinner);
+// let sum = start - spending + earning;
+// console.log(sum);
+
+// document.body.style.backgroundColor = "pink"; 
+
+//                        lesson 2
+
+
+// console.log(`items (${2 + 2}): $${(2095 * 2 + 799 * 2 ) / 100} 
+// \nshipping & handling: $${(499 + 499) / 100} 
+// \ntotal before tax: $${(2095 * 2 + 799 * 2 + 499 + 499) / 100} 
+// \nestimated tax(10%): $${((2095 * 2 + 799 * 2 + 499 + 499) / 100 * 0.1).toFixed(2)} 
+// \norder total: $${((2095 * 2 + 799 * 2 + 499 + 499) / 100) + 6.79}`);
+
+
+//                     lesson idk (update its 5)
+
+// let variable1 = 3;
+// console.log(variable1);
+
+// let calculation = 2 + 2;
+// console.log(calculation);
+// console.log(calculation + 2);
+
+// let message = 'hello';
+// console.log(message);
+
+// variable1 = 5;
+// console.log(variable1);
+
+//                  lesson 6??? maybe still 5 idk
+
+console.log('hi elen');
